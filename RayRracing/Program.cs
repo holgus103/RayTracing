622bec2780bad4799b9508a5cb9bce260ce1cfb0
holgus103 <kubasuchan@hotmail.com>
2018-01-04 01:54:28 +0100

    Imported 3DGraphics.Core

--- RayRracing/Program.cs
+++ RayRracing/Program.cs
@@ -14,7 +14,7 @@ namespace RayRracing
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new RayTracing())
                 game.Run();
         }
     }
---
+++ RayRracing/RayTracing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using _3DGraphics.Core;
+
+namespace RayRracing
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class RayTracing : GameBase
+    {
+
+    }
+}
